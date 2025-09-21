875259af35301c517574d44a2ef389ee7877629b
Vitryk Nika <v.karpinska@ukr.net>
2025-09-21 05:27:23 +0300

    new files

---
+++ src/App/App.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import css from './App.module.css';
+
+import CafeInfo from './components/CafeInfo/CafeInfo';
+import VoteOptions from './components/VoteOptions/VoteOptions';
+import VoteStats from './components/VoteStats/VoteStats';
+import type { Votes, VoteType } from '../types/votes';
+import Notification from './components/Notification/Notification';
+
+
+export default function App() {
+  const [votes, setVotes] = useState<Votes>({ good: 0, neutral: 0, bad: 0 });
+
+  function handleVote(type: VoteType) {
+    setVotes(prev => ({ ...prev, [type]: prev[type] + 1 }));
+  }
+
+  function resetVotes() {
+    setVotes({ good: 0, neutral: 0, bad: 0 });
+  }
+
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+  const positiveRate = totalVotes ? Math.round((votes.good / totalVotes) * 100) : 0;
+
+  return (
+   <div className={css.app}>
+    <CafeInfo />
+
+    <VoteOptions
+      onVote={handleVote}
+      onReset={resetVotes}
+      canReset={totalVotes > 0}
+    />
+
+    {totalVotes > 0 ? (
+      <VoteStats
+        votes={votes}
+        totalVotes={totalVotes}
+        positiveRate={positiveRate}
+      />
+    ) : (
+      <Notification />
+    )}
+  </div>
+  );
+}
---
+++ src/App/components/VoteOptions/VoteOptions.tsx
@@ -0,0 +1,36 @@
+import css from './VoteOptions.module.css';
+
+import type { VoteType } from '../../../types/votes';
+
+interface VoteOptionsProps {
+  onVote: (type: VoteType) => void;
+  onReset: () => void;
+  canReset: boolean;
+}
+
+export default function VoteOptions({ onVote, onReset, canReset }: VoteOptionsProps) {
+  return (
+    <div className={css.container}>
+      <button className={css.button} onClick={() => onVote('good')} aria-label="Vote good">
+        Good
+      </button>
+      <button className={css.button} onClick={() => onVote('neutral')} aria-label="Vote neutral">
+        Neutral
+      </button>
+      <button className={css.button} onClick={() => onVote('bad')} aria-label="Vote bad">
+        Bad
+      </button>
+
+      {canReset && (
+        <button
+          className={`${css.button} ${css.reset}`}
+          type="button"
+          onClick={onReset}
+          
+        >
+          Reset
+        </button>
+      )}
+    </div>
+  );
+}
